78a3fefc6a7ad7329cf5649479e34f45dbd1c9bb
kylelambert001 <kylelambert001@gmail.com>
2020-11-19 22:24:34 +1030

    Added loading spinner component

--- client/src/components/app/app.js
+++ client/src/components/app/app.js
@@ -1,9 +1,9 @@
 import React from "react";
-import { Switch, Route, Redirect } from "react-router-dom";
+// import { Switch, Route, Redirect } from "react-router-dom";
 
-import Home from "../../pages/home/home";
-import Games from "../../pages/games/games";
-import Error from "../../pages/error/error";
+// import Home from "../../pages/home/home";
+// import Games from "../../pages/games/games";
+// import Error from "../../pages/error/error";
 
 import Navbar from "../../components/navbar/navbar";
 import LoginModal from "../../components/login-modal/login-modal";
