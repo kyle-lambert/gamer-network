ff568eeb59abe8f681e5b02716706b9c0a0ffd07
kylelambert001 <kylelambert001@gmail.com>
2021-01-26 21:45:15 +1030

    Added 404 message to error page

--- client/src/pages/ErrorPage/ErrorPage.js
+++ client/src/pages/ErrorPage/ErrorPage.js
@@ -1,8 +1,21 @@
 import React from "react";
 import "./ErrorPage.scss";
 
+import PageHeader from "../../components/shared/PageHeader/PageHeader";
+import LinkButton from "../../components/shared/LinkButton/LinkButton";
+
 function ErrorPage(props) {
-  return <div className="ErrorPage">Error page</div>;
+  return (
+    <div className="ErrorPage">
+      <PageHeader
+        heading="404 Not Found"
+        subheading="Sorry, the page you are looking for does not exist."
+      />
+      <div className="ErrorPage__redirect">
+        <LinkButton to="/">Redirect To Home Page</LinkButton>
+      </div>
+    </div>
+  );
 }
 
 export default ErrorPage;
