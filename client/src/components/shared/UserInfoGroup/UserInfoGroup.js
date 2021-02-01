73bb8df9cb576779a8d259756d30c66e2816e15e
kylelambert001 <kylelambert001@gmail.com>
2021-02-01 13:42:54 +1030

    Shared component for displaying user info

---
+++ client/src/components/shared/UserInfoGroup/UserInfoGroup.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./UserInfoGroup.scss";
+
+import Avatar from "../Avatar/Avatar";
+
+function UserInfoGroup({ user }) {
+  return (
+    <div className="UserInfoGroup">
+      <Link to={`/profile/${user._id}`} className="UserInfoGroup__avatar">
+        <Avatar user={user} />
+      </Link>
+      <div className="UserInfoGroup__info">
+        <span className="UserInfoGroup__name">{user ? user.firstName : "No author"}</span>
+        <span className="UserInfoGroup__created-at">{user ? user.createdAt : "No date"}</span>
+      </div>
+    </div>
+  );
+}
+
+export default UserInfoGroup;
