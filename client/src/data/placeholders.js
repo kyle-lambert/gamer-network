de6c8a2ee54a77928e1baa21ade81410032611eb
kylelambert001 <kylelambert001@gmail.com>
2021-02-02 16:29:52 +1030

    Placeholder comments for development, removed in production.

--- client/src/data/placeholders.js
+++ client/src/data/placeholders.js
@@ -3,6 +3,32 @@ import palmTrees from "../assets/palm-trees.jpg";
 import pridePerson from "../assets/pride-person.jpg";
 import mountain from "../assets/mountain.jpg";
 
+export const comments = [
+  {
+    _id: 1,
+    author: {
+      _id: 10,
+      firstName: "Bobby",
+      lastName: "Daws",
+      createdAt: "7:58pm",
+      hexColor: "#A3E4D7",
+    },
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis",
+  },
+  {
+    _id: 2,
+    author: {
+      _id: 11,
+      firstName: "Steven",
+      lastName: "King",
+      createdAt: "2:11am",
+      avatar: mountain,
+    },
+    text:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis ducimus doloremque labore obcaecati aut ratione maiores .",
+  },
+];
+
 export const posts = [
   {
     _id: 1,
@@ -10,7 +36,7 @@ export const posts = [
       _id: 10,
       firstName: "Kyle",
       lastName: "Lambert",
-      createdAt: "10/07/2020",
+      createdAt: "1",
       hexColor: "#A3E4D7",
     },
     text:
@@ -23,7 +49,7 @@ export const posts = [
       avatar: avatar,
       firstName: "Troy",
       lastName: "Lambert",
-      createdAt: "13th December, 2020",
+      createdAt: "2",
       hexColor: "#ffffff",
     },
     text:
@@ -37,7 +63,47 @@ export const posts = [
       avatar: avatar,
       firstName: "Bobby",
       lastName: "Stev",
-      createdAt: "1st July, 2021",
+      createdAt: "3",
+      hexColor: "#ffffff",
+    },
+    text:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis ducimus doloremque labore obcaecati aut ratione maiores animi cupiditate veniam quae architecto sed inventore, itaque dolore. Accusantium itaque quo debitis eveniet?Nulla doloremque suscipit eius quis animi deleniti cupiditate sint provident impedit. Ipsam ullam doloremque temporibus repellat dicta repudiandae rerum enim quam, consectetur perferendis corrupti officia. Voluptatum non et architecto repellendus.",
+    image: palmTrees,
+  },
+  {
+    _id: 4,
+    author: {
+      _id: 10,
+      firstName: "Kyle",
+      lastName: "Lambert",
+      createdAt: "4",
+      hexColor: "#A3E4D7",
+    },
+    text:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis ducimus doloremque labore obcaecati aut ratione maiores animi cupiditate veniam quae architecto sed inventore, itaque dolore. Accusantium itaque quo debitis eveniet?Nulla doloremque suscipit eius quis animi deleniti cupiditate sint provident impedit. Ipsam ullam doloremque temporibus repellat dicta repudiandae rerum enim quam, consectetur perferendis corrupti officia. Voluptatum non et architecto repellendus.",
+  },
+  {
+    _id: 5,
+    author: {
+      _id: 11,
+      avatar: avatar,
+      firstName: "Troy",
+      lastName: "Lambert",
+      createdAt: "5",
+      hexColor: "#ffffff",
+    },
+    text:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis ducimus doloremque labore obcaecati aut ratione maiores animi cupiditate veniam quae architecto sed inventore, itaque dolore. Accusantium itaque quo debitis eveniet?Nulla doloremque suscipit eius quis animi deleniti cupiditate sint provident impedit. Ipsam ullam doloremque temporibus repellat dicta repudiandae rerum enim quam, consectetur perferendis corrupti officia. Voluptatum non et architecto repellendus.",
+    image: pridePerson,
+  },
+  {
+    _id: 6,
+    author: {
+      _id: 12,
+      avatar: avatar,
+      firstName: "Bobby",
+      lastName: "Stev",
+      createdAt: "6",
       hexColor: "#ffffff",
     },
     text:
