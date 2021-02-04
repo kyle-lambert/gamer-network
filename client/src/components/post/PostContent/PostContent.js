065233b2c3d26dcfe2154f847bd6a5d3a643707b
kylelambert001 <kylelambert001@gmail.com>
2021-02-04 15:51:39 +1030

    Fix image alt warning

--- client/src/components/post/PostContent/PostContent.js
+++ client/src/components/post/PostContent/PostContent.js
@@ -5,7 +5,7 @@ function PostContent({ post }) {
   return post ? (
     <div className="PostContent">
       <p className="PostContent__text">{post.text ? post.text : "No text"}</p>
-      {post.image && <img src={post.image} alt="Post Image" className="PostContent__image" />}
+      {post.image && <img src={post.image} alt="Post content" className="PostContent__image" />}
     </div>
   ) : null;
 }
