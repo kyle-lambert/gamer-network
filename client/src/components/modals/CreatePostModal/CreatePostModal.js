111649e2b6eca7e59447c4a650fe6456314d5a42
kylelambert001 <kylelambert001@gmail.com>
2021-02-08 14:50:46 +1030

    Make create post form controlled.

--- client/src/components/modals/CreatePostModal/CreatePostModal.js
+++ client/src/components/modals/CreatePostModal/CreatePostModal.js
@@ -1,8 +1,10 @@
 import React from "react";
+import axios from "axios";
 import { useDispatch, useSelector } from "react-redux";
 import "./CreatePostModal.scss";
 
 import { hideCurrentModalAction } from "../../../store/actions/modalActions";
+import { createNewPostAction } from "../../../store/actions/createPostActions";
 
 import FormTextArea from "../../forms/FormTextArea/FormTextArea";
 import Button from "../../shared/Button/Button";
@@ -16,8 +18,13 @@ import ModalFooter from "../ModalFooter/ModalFooter";
 
 function CreatePostModal(props) {
   const textAreaRef = React.useRef(null);
+  const [state, setState] = React.useState({
+    text: "",
+    image: "",
+  });
   const dispatch = useDispatch();
   const { user } = useSelector((state) => state.auth);
+  const { createPostLoading, createPostError } = useSelector((state) => state.createPost);
   const closeCreatePostModal = () => dispatch(hideCurrentModalAction());
 
   React.useEffect(() => {
@@ -25,6 +32,23 @@ function CreatePostModal(props) {
       textAreaRef.current.focus();
     }
   }, []);
+
+  const handleChange = (e) => {
+    console.log(e.target.name);
+    setState((prev) => {
+      return {
+        ...prev,
+        [e.target.name]: e.target.value,
+      };
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (state.text) {
+      dispatch(createNewPostAction(state));
+    }
+  };
 
   return (
     <Modal>
@@ -35,13 +59,19 @@ function CreatePostModal(props) {
           <UserInfoGroup user={user} />
         </div>
         <div className="CreatePostModal">
-          <form className="CreatePostModal__form">
-            <div className="CreatePostModal__form-line CreatePostModal__form-line--textarea">
-              <FormTextArea ref={textAreaRef} placeholder="Tell us all what you're thinking..." />
+          <form onSubmit={handleSubmit} className="CreatePostModal__form">
+            <div className="CreatePostModal__form-line">
+              <FormTextArea
+                ref={textAreaRef}
+                name="text"
+                onChange={handleChange}
+                value={state.text}
+                placeholder="Tell us all what you're thinking..."
+              />
             </div>
             {/* <div className="CreatePostModal__form-line CreatePostModal__form-line--upload"></div> */}
-            <div className="CreatePostModal__form-line CreatePostModal__form-line--submit">
-              <Button color="indigo" width="full">
+            <div className="CreatePostModal__form-line">
+              <Button type="submit" color="indigo" isLoading={createPostLoading} width="full">
                 Create Post
               </Button>
             </div>
