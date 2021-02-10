00e6888e71beecc965e878be3280b84f5f8bbd57
kylelambert001 <kylelambert001@gmail.com>
2021-02-10 17:58:43 +1030

    Write comment functionality for redux store

--- client/src/store/actions/postActions.js
+++ client/src/store/actions/postActions.js
@@ -3,6 +3,8 @@ import { postTypes } from "../types";
 import { createAlert } from "./alertActions";
 import api from "../../data/api";
 
+const COMMENT_ERROR = "No comment provided";
+const POST_ID_ERROR = "Post ID is required";
 const REQUEST_ERROR = "No response from server";
 const REQUEST_FAILED = "Unable to make request";
 
@@ -19,7 +21,77 @@ const loadPostsFailure = () => ({
   type: postTypes.LOAD_POSTS_FAILURE,
 });
 
+const addCommentRequest = (id) => ({
+  type: postTypes.ADD_COMMENT_REQUEST,
+  payload: id,
+});
+
+const addCommentSuccess = (id) => ({
+  type: postTypes.ADD_COMMENT_SUCCESS,
+  payload: id,
+});
+
+const addCommentFailure = (id) => ({
+  type: postTypes.ADD_COMMENT_FAILURE,
+  payload: id,
+});
+
 export const initialisePostReducer = () => ({ type: postTypes.INITIALISE_POST_REDUCER });
+
+export const addCommentToPost = ({ id, comment, token }) => {
+  return async (dispatch) => {
+    if (!comment) {
+      return dispatch(createAlert(COMMENT_ERROR, true));
+    }
+    if (!id) {
+      return dispatch(createAlert(POST_ID_ERROR, true));
+    }
+
+    try {
+      dispatch(addCommentRequest(id));
+      const config = {
+        method: "post",
+        url: `/posts/comment/${id}`,
+        data: {
+          text: comment,
+        },
+        cancelToken: token,
+      };
+
+      const res = await api(config);
+      const data = res?.data;
+
+      const payload = {
+        id,
+        comments: data.comments,
+      };
+
+      dispatch(addCommentSuccess(payload));
+
+      if (!data) {
+        throw new Error("No results found in response");
+      }
+    } catch (err) {
+      if (axios.isCancel(err)) {
+        console.log("Axios request cancelled");
+      } else {
+        dispatch(addCommentFailure(id));
+        if (err.response) {
+          const errors = err.response?.data?.errors;
+          if (errors) {
+            errors.forEach((error) => {
+              dispatch(createAlert(error.msg, true));
+            });
+          }
+        } else if (err.request) {
+          dispatch(createAlert(REQUEST_ERROR, true));
+        } else {
+          dispatch(createAlert(REQUEST_FAILED, true));
+        }
+      }
+    }
+  };
+};
 
 export const loadPosts = (page, token) => {
   return async (dispatch) => {
--- client/src/store/reducers/postReducer.js
+++ client/src/store/reducers/postReducer.js
@@ -4,6 +4,7 @@ const initState = {
   posts: [],
   postsLoading: false,
   postsError: false,
+  commentsLoading: [],
 };
 
 function postReducer(state = initState, action) {
@@ -26,6 +27,32 @@ function postReducer(state = initState, action) {
         ...state,
         postsLoading: false,
         postsError: true,
+      };
+    }
+    case postTypes.ADD_COMMENT_REQUEST: {
+      return {
+        ...state,
+        commentsLoading: [...state.commentsLoading, action.payload],
+      };
+    }
+    case postTypes.ADD_COMMENT_SUCCESS: {
+      const updatedPosts = state.posts.reduce((acc, post) => {
+        if (post._id === action.payload.id) {
+          post.comments = action.payload.comments;
+        }
+        return acc.concat(post);
+      }, []);
+
+      return {
+        ...state,
+        posts: updatedPosts,
+        commentsLoading: state.commentsLoading.filter((id) => id !== action.payload.id),
+      };
+    }
+    case postTypes.ADD_COMMENT_FAILURE: {
+      return {
+        ...state,
+        commentsLoading: state.commentsLoading.filter((id) => id !== action.payload),
       };
     }
     case postTypes.INITIALISE_POST_REDUCER: {
