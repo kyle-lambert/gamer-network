ded45aed2b2218a4bb6e30eb40208c4f6d3a565d
kylelambert001 <kylelambert001@gmail.com>
2021-02-11 14:23:35 +1030

    Update circle icon references.

--- client/src/components/post/PostEngagement/PostEngagement.js
+++ client/src/components/post/PostEngagement/PostEngagement.js
@@ -4,7 +4,7 @@ import "./PostEngagement.scss";
 import { ReactComponent as ThumbUp } from "../../../assets/icons/thumb_up.svg";
 import { ReactComponent as Comments } from "../../../assets/icons/comments.svg";
 
-import CubeIcon from "../../shared/CubeIcon/CubeIcon";
+import CircleIcon from "../../shared/CircleIcon/CircleIcon";
 
 function PostEngagement({ comments, likes }) {
   const commentCount = Array.isArray(comments) ? comments.length : null;
@@ -15,17 +15,17 @@ function PostEngagement({ comments, likes }) {
       <div className="PostEngagement__group">
         <span className="PostEngagement__count">{likeCount ? likeCount : 0}</span>
         <span className="PostEngagement__icon">
-          <CubeIcon color="indigo" size="small">
+          <CircleIcon circleColor="indigo" iconColor="indigo" iconSize="small">
             <ThumbUp />
-          </CubeIcon>
+          </CircleIcon>
         </span>
       </div>
       <div className="PostEngagement__group">
         <span className="PostEngagement__count">{commentCount ? commentCount : 0}</span>
         <span className="PostEngagement__icon">
-          <CubeIcon color="indigo" size="small">
+          <CircleIcon circleColor="indigo" iconColor="indigo" iconSize="small">
             <Comments />
-          </CubeIcon>
+          </CircleIcon>
         </span>
       </div>
     </div>
