6f2c5cf0da369aeba3292bcfa784c0cf29c2acfb
kylelambert001 <kylelambert001@gmail.com>
2021-02-11 14:24:04 +1030

    Add delete post button to comment bubble.

--- client/src/components/post/PostComment/PostComment.js
+++ client/src/components/post/PostComment/PostComment.js
@@ -1,12 +1,23 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 import "./PostComment.scss";
 
 import { getFullName } from "../../../utils/helpers";
 
+import { ReactComponent as DeleteIcon } from "../../../assets/icons/delete.svg";
+import ButtonSpinner from "../../shared/ButtonSpinner/ButtonSpinner";
+
 import Avatar from "../../shared/Avatar/Avatar";
+import CircleIcon from "../../shared/CircleIcon/CircleIcon";
 
 function PostComment({ comment }) {
+  const { user } = useSelector((state) => state.authReducer);
+
+  const isAuthor = React.useMemo(() => {
+    return user._id === comment.author._id;
+  }, [user._id, comment.author._id]);
+
   return (
     <li className="PostComment">
       <Link to={`/profile/${comment.author._id}`} className="PostComment__avatar-link">
@@ -16,7 +27,16 @@ function PostComment({ comment }) {
         <span className="PostComment__author">
           {getFullName(comment.author.firstName, comment.author.lastName)}
         </span>
-        <div className="PostComment__text">{comment.text}</div>
+        <div className="PostComment__bubble">
+          <div className="PostComment__text">{comment.text}</div>
+          {isAuthor && (
+            <button className="PostComment__remove">
+              <CircleIcon circleColor="red" iconColor="red" iconSize="small">
+                {true ? <DeleteIcon /> : <ButtonSpinner color="red" />}
+              </CircleIcon>
+            </button>
+          )}
+        </div>
       </div>
     </li>
   );
